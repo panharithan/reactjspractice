9ce047a61b1e7549cd7881c929edd8f28bbae848
panharithan <an.panharith111@gmail.com>
2020-04-25 13:11:16 +0700

    create componenet 2

--- themandashboard/src/App.js
+++ themandashboard/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'; // import Greet component from Greet.js
-
+import {Greet2} from './components/Greet'; // another way to import a component
 
 function App() {
   return (
     <div className="App">
       <Greet/>
+      <Greet2/>
+      
     </div>
   );
 }
--- themandashboard/src/components/Greet.js
+++ themandashboard/src/components/Greet.js
@@ -5,4 +5,6 @@ function Greet()    // this is a component with name Greet
     return <h1>Hello The Man</h1>
 }
 
+export const Greet2 = () => <h1>Hello The Man 2</h1>; // create another component
+
 export default Greet;
