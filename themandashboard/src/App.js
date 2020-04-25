0542bb0cce4fdf67be0a42df06b37a8f60efdb1c
panharithan <an.panharith111@gmail.com>
2020-04-25 13:20:58 +0700

    create a class component

--- themandashboard/src/App.js
+++ themandashboard/src/App.js
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'; // import Greet component from Greet.js
 import {Greet2} from './components/Greet'; // another way to import a component
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
       <Greet/>
       <Greet2/>
+      <Welcome/>
+      
       
     </div>
   );
